dc8c2a35b959b3eeab260c1b4473fd8922aff47f
kolcz <pkonczyk1@gmail.com>
2016-08-03 01:51:19 +0200

    Create PythonVillage3.py

---
+++ PythonVillage3.py
@@ -0,0 +1,8 @@
+z=' '
+s='gfaUDAntaresiaXHqkSMz4xe8OaK89UK4HgPetSQDqtmh7LWyBvZcxlttSDdq81azafGnzJKUdmzUujYcFxzmJYfVr19Td5LJPi2Pfzwz0CeV89F1HVwgeT21koBCXQHFfA2paFCJRGf7pholeterg8NEsRSPSUW2bt3z7q44i6hJI63XOxlinm0fq7JYG0G.'
+a=5
+b=13
+c=141
+d=148
+r=s[a:b+1]+z+s[c:d+1]
+print r
